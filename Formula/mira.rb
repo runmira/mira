48ef6f1f35d875f02ac074f83b3adcf1178fc7ba
DamilolaDami <flutterdami@gmail.com>
2026-09-20 01:30:58 +0100

    Format Rust sources

--- Formula/mira.rb
+++ Formula/mira.rb
@@ -9,7 +9,7 @@
 class Mira < Formula
   desc "Open-source coding agent you run yourself, with the model you choose"
   homepage "https://github.com/runmira/mira"
-  version "0.3.3"
+  version "0.3.4"
   license "Apache-2.0"
 
   on_macos do
