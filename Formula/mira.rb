1d82595c6edbe7893be7ff497931d0450e79dfa7
DamilolaDami <flutterdami@gmail.com>
2026-09-20 01:27:33 +0100

    updates

--- Formula/mira.rb
+++ Formula/mira.rb
@@ -9,30 +9,30 @@
 class Mira < Formula
   desc "Open-source coding agent you run yourself, with the model you choose"
   homepage "https://github.com/runmira/mira"
-  version "0.3.2"
+  version "0.3.3"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-arm64.tar.gz"
-      sha256 "238b870e51a1f76a98bad589ef88e2627b10a72ce9db4005c0991c09c5215307"
+      sha256 "REPLACE_ME"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-x86_64.tar.gz"
-      sha256 "dbe6bc0b82ec3e16e77880d504592e91f3fc5df937a1cc9288a871811a6d2c5c"
+      sha256 "REPLACE_ME"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-arm64.tar.gz"
-      sha256 "bcb6eb5ffd572aab2a31f1afa0a62b3310e3b627d46b3c2f1d7cc52084a5acaa"
+      sha256 "REPLACE_ME"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-x86_64.tar.gz"
-      sha256 "15854aa86885ff0d3363c88d34286bf7363a707ef3b933384e2a8f30e4b7fb33"
+      sha256 "REPLACE_ME"
     end
   end
 
