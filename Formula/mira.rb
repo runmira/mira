182bd05b436a185c75dfbd514ee4ed00f97793eb
DamilolaDami <flutterdami@gmail.com>
2026-09-25 15:25:38 +0100

    fix: correct sha256 checksums for v0.3.9 homebrew formula

--- Formula/mira.rb
+++ Formula/mira.rb
@@ -15,24 +15,24 @@ class Mira < Formula
   on_macos do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-arm64.tar.gz"
-      sha256 "ef4f16c883f092f1c843d4be93ee6270651c16faf406ecbef2275da528bb00d8"
+      sha256 "20386540bdba0c40766ed50cbfca1b85d9996d8126ea7a91ac3c76827dcf7bdc"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-x86_64.tar.gz"
-      sha256 "53f8ed92a5bddf97f85ab34d6b2c5e471b0ab5d7a3370001815d06aece9dfa35"
+      sha256 "5be78a8ccd874793e3c42ce6b1aaa0cbad8feede56a80bc7704e12e53607cb3e"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-arm64.tar.gz"
-      sha256 "19ba2617039dced9ac62d070da340bbb63b5a2f43f7b95baf4f02506a22fc8d2"
+      sha256 "446e2346fd7c6ccc04f2eb2b250bff8596b9c9fdcc234e60ee793ca2e0b287de"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-x86_64.tar.gz"
-      sha256 "ef6a01f0449ac8f57347c4c472681a9ba1c727d02d128ad76d6cb15cd33765e3"
+      sha256 "4ce8174c3e4d01a0857dc13d14b4ed4cab9a66cd323309c1d53216bf1078ea5c"
     end
   end
 
