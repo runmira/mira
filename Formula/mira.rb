5e0f76b9ede4ba75958af9bafb2eaaf362d6f1a7
DamilolaDami <flutterdami@gmail.com>
2026-09-25 12:55:14 +0100

    Formula: fill in v0.4.0 SHA256s

--- Formula/mira.rb
+++ Formula/mira.rb
@@ -15,24 +15,24 @@ class Mira < Formula
   on_macos do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-arm64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "ef4f16c883f092f1c843d4be93ee6270651c16faf406ecbef2275da528bb00d8"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-darwin-x86_64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "53f8ed92a5bddf97f85ab34d6b2c5e471b0ab5d7a3370001815d06aece9dfa35"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-arm64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "19ba2617039dced9ac62d070da340bbb63b5a2f43f7b95baf4f02506a22fc8d2"
     end
 
     on_intel do
       url "https://github.com/runmira/mira/releases/download/v#{version}/mira-linux-x86_64.tar.gz"
-      sha256 "REPLACE_ME"
+      sha256 "ef6a01f0449ac8f57347c4c472681a9ba1c727d02d128ad76d6cb15cd33765e3"
     end
   end
 
